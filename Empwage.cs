69a44379b329c1f43ae0546c21580deb1630892b
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-12 12:48:10 +0530

    Adding Welcome Message

---
+++ Empwage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EmployeeWageCompute058
+{
+    class EmployeeWageCompute
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Empwage");
+
+        }
+    }
+}
+           

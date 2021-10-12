584cd0a092841d977c42173f0acdd86676e07667
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-12 12:53:47 +0530

    Adding Emp PresentorAbsent

---
+++ Empwage1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+    namespace HelloWorld
+    {
+        public class UC1_EmpAttendance
+        {
+            public static void EmpAttendance()
+            {
+                int IS_FULL_Time = 1;
+                Random random = new Random();
+                int result = random.Next(0, 2);
+                if (result == IS_FULL_Time)
+                    Console.WriteLine("Employee is Present");
+                else
+                    Console.WriteLine("Employee is absent");
+                Console.ReadLine();
+            }
+        }
+    }
+  

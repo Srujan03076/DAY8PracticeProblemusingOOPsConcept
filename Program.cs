6be197933799d08b6680c538df1e7c31bff03dd8
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-14 18:48:33 +0530

    Adding Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1_EmpAttendance.EmpAttendance();
+            //UC2_EmpWage.EmpWage();
+            //UC3_PartTimeEmp.PartTimeEmp();
+            //UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
+            //UC5_WageForMonth.WageForMonth();
+            //UC6_WageTillNumOfWorkingDays.WageTillNumOfWorkingDays();
+            //UC7_EmpWageUsingClassMethod.EmpWageUsingClassMethod();
+        }
+    }
+
+}

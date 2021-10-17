17f4749e93e77fff4f71c4ffdba6d4b3adb9487d
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-17 11:16:27 +0530

    Adding Program.cs

--- Program.cs
+++ Program.cs
@@ -19,6 +19,28 @@ namespace HelloWorld
             //UC7_EmpWageUsingClassMethod.EmpWageUsingClassMethod();
             //UC8_EmpWageForMultipleCompanies.computeEmpWage("DMart", 20, 2, 10);
             //UC8_EmpWageForMultipleCompanies.computeEmpWage("Reliance", 10, 4, 20
+
+
+            //#region for Uc9 Implementation
+
+            //EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            //EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            //dMart.computeEmpWage();
+            //Console.WriteLine(dMart.toString());
+            //reliance.computeEmpWage();
+            //Console.WriteLine(reliance.toString());
+
+            //#endregion
+
+            #region for UC10
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+            #endregion
+
+            Console.ReadLine();
+
         }
     }
 }
